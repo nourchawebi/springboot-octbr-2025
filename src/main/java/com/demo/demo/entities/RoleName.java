d558336c84c9095b031f2fac077493fb0cc7125f
nourchawebi <nourelhouda.chawebi@esprit.tn>
2025-10-19 10:00:38 +0100

    seance3tables

---
+++ src/main/java/com/demo/demo/entities/RoleName.java
@@ -0,0 +1,5 @@
+package com.demo.demo.entities;
+
+public enum RoleName {
+    ADMIN,USER
+}
--- src/main/java/com/demo/demo/entities/UserEntity.java
+++ src/main/java/com/demo/demo/entities/UserEntity.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Data
 @Table(name="users")
@@ -25,4 +28,17 @@ public class UserEntity {
     @Column(nullable = false,unique = true)
     private String username;
     private String confirmPassword;
+//    @ManyToMany
+//    private Set<Role> role;
+//    @JoinTable(name="userrole", joinColumns = @JoinColumn(name="email"), inverseJoinColumns = @JoinColumn(name="idrole"))
+    @ManyToOne
+    @JoinColumn(name="role_id")
+    private Role role;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL, fetch=FetchType.LAZY)
+    private List<Post> posts;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> comments;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="likeId", referencedColumnName = "id")
+    private Likes likes;
 }
